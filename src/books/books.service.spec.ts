1c46efe0eec90e142b2a6bf005dd0a131f496c5c
Tammy <90193520+tyfreire@users.noreply.github.com>
2024-07-18 17:48:40 +0200

    Starts  books service test suit

--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -1,18 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BooksService } from './books.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Book } from './book.entity';
+
 
 describe('BooksService', () => {
-  let service: BooksService;
+  let bookservice: BooksService;
 
+  const mockBookRepository ={
+    create: jest.fn().mockImplementation(dto =>dto),
+    save: jest.fn().mockImplementation(book => Promise.resolve({id: Math.random(), ...book}))
+
+  }
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BooksService],
+      providers: [BooksService, {
+        provide:getRepositoryToken(Book),
+        useValue: mockBookRepository
+      }],
     }).compile();
 
-    service = module.get<BooksService>(BooksService);
+    bookservice = module.get<BooksService>(BooksService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(bookservice).toBeDefined();
   });
+
+
+  it('should persist a book in the DB and return it', async ()=>{
+    const dto ={
+      title: "Book1",
+      author: "Myself",
+      pages: 150,
+      language: "ENGLISH",
+      status: "READ",
+      isbn: 1212354465
+    }
+
+    let book =  mockBookRepository.create(dto)
+    expect(book.title).toBe("Book1")
+
+    const book_1 = await mockBookRepository.save(book)
+    expect(await book_1.author).toBe("Myself")
+
+
+  })
 });
