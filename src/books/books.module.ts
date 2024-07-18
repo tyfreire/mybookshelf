b7e20bd23b04a43f80387cfe82d9556501cd6bfe
Tammy <90193520+tyfreire@users.noreply.github.com>
2024-07-18 17:48:05 +0200

    Updates books module

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
+import { TypeOrmModule} from '@nestjs/typeorm';
+import { Book } from './book.entity';
+
+
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Book])],
   controllers: [BooksController],
   providers: [BooksService]
 })
