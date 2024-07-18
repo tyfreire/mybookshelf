8417524574cc2e84e5b89aa37845b4a6fa7d8b17
Tammy <90193520+tyfreire@users.noreply.github.com>
2024-07-18 17:49:04 +0200

    Implements Books service create function

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Book } from './book.entity';
+import {Repository} from 'typeorm'
+import { CreateBookDto } from './dtos/create-book.dto';
 
 @Injectable()
-export class BooksService {}
+export class BooksService{
+  constructor(@InjectRepository(Book) private repo:Repository<Book>){}
+
+  async create(bookDto: CreateBookDto){
+    const book = this.repo.create(bookDto)
+    return  this.repo.save(book)
+  }
+
+  // findOne(){}
+  // update(){}
+  // remove(){}
+}
