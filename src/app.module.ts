5d4d9e1d052b82a86ed4c3ee431191ecf5226733
Tammy <90193520+tyfreire@users.noreply.github.com>
2024-07-22 14:39:40 +0200

    Correct set to connect to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import {TypeOrmModule} from '@nestjs/typeorm'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
-import { Book } from './books/book.entity';
 
 @Module({
   imports: [
@@ -13,8 +12,8 @@ import { Book } from './books/book.entity';
       port: 5432,
       username: 'postgres',
       password: 'postgres',
-      database: 'books',
-      entities:[Book],
+      database: 'mybookshelf',
+      entities:['dist/**/*.entity.js'],
       synchronize: true,
     }),
     BooksModule
