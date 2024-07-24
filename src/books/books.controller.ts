558a7ea38b8c846e6d0da4991cbbcccece387e4c
Tammy <90193520+tyfreire@users.noreply.github.com>
2024-07-24 17:56:56 +0200

    Implements create function on books controller

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { BooksService } from './books.service';
+import { CreateBookDto } from './dtos/create-book.dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private bookService: BooksService){}
+
+  @Post()
+  async createBook(@Body() body: CreateBookDto){
+    const book = await this.bookService.create(body)
+  }
+}
