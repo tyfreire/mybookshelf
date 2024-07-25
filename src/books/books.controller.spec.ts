a510886d3e05471fa4ab30a7f7e2f59a6f81220c
Tammy <90193520+tyfreire@users.noreply.github.com>
2024-07-25 16:49:26 +0200

    WIP controller test

--- src/books/books.controller.spec.ts
+++ src/books/books.controller.spec.ts
@@ -1,18 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BooksController } from './books.controller';
+import { BooksService } from './books.service';
+import { APP_PIPE } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 
 describe('BooksController', () => {
   let controller: BooksController;
+  let service: BooksService;
+
+  const mockBookRepository ={
+    create: jest.fn().mockImplementation(dto =>dto),
+    save: jest.fn().mockImplementation(book => Promise.resolve({id: Math.random(), ...book}))
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BooksController],
+      providers:[{
+        provide: BooksService,
+        useValue: mockBookRepository
+      },
+      { provide: APP_PIPE,
+        useClass: ValidationPipe,
+
+    }]
     }).compile();
 
     controller = module.get<BooksController>(BooksController);
+    service = module.get<BooksService>(BooksService)
   });
 
-  it('should be defined', () => {
+   afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should be defined', async() => {
     expect(controller).toBeDefined();
   });
-});
+
+})
