394e4ad2428e7fa8eaccf57a1d546dd5f8cbfd74
Tammy <90193520+tyfreire@users.noreply.github.com>
2024-07-26 17:13:54 +0200

    Implements find all test

--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -3,19 +3,56 @@ import { BooksService } from './books.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Book } from './book.entity';
 
+
 describe('BooksService', () => {
   let bookservice: BooksService;
 
-  const mockBookRepository ={
-    create: jest.fn().mockImplementation(dto =>dto),
-    save: jest.fn().mockImplementation(book => Promise.resolve({id: Math.random(), ...book}))
-  }
+  const mockBookRepository = {
+    create: jest.fn().mockImplementation((dto) => dto),
+    save: jest
+      .fn()
+      .mockImplementation((book) =>
+        Promise.resolve({ id: Math.random(), ...book }),
+      ),
+    findAll: jest.fn().mockImplementation(() => [
+      {
+        title: 'The Darkest mind',
+        author: 'Alexandra Braken',
+        pages: 528,
+        language: 'ENGLISH',
+        status: 'READ',
+        isbn: 1212354465,
+        id: 1,
+      },
+      {
+        title: 'Never Fade',
+        author: 'Alexandra Braken',
+        pages: 507,
+        language: 'ENGLISH',
+        status: 'READ',
+        isbn: 1212354466,
+        id: 2,
+      },
+      {
+        title: 'In the afterlight',
+        author: 'Alexandra Braken',
+        pages: 544,
+        language: 'ENGLISH',
+        status: 'READ',
+        isbn: 1212354467,
+        id: 3,
+      },
+    ]),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BooksService, {
-        provide:getRepositoryToken(Book),
-        useValue: mockBookRepository
-      }],
+      providers: [
+        BooksService,
+        {
+          provide: getRepositoryToken(Book),
+          useValue: mockBookRepository,
+        },
+      ],
     }).compile();
 
     bookservice = module.get<BooksService>(BooksService);
@@ -25,8 +62,7 @@ describe('BooksService', () => {
     expect(bookservice).toBeDefined();
   });
 
-
-  it('should persist a book in the DB and return it', async ()=>{
+  it('persists a book in the DB and returns it', async () => {
     const dto = {
       title: 'Book1',
       author: 'Myself',
@@ -41,5 +77,17 @@ describe('BooksService', () => {
 
     const book_1 = await mockBookRepository.save(book);
     expect(await book_1.author).toBe('Myself');
-  })
+  });
+
+  it('finds all books persisted in the db and returns them', async () => {
+    const result = await mockBookRepository.findAll();
+
+    expect(result[0].author).toEqual('Alexandra Braken');
+    expect(result[1].title).toEqual('Never Fade');
+    expect(result[2].id).toBe(3);
+
+    expect(result[3]).toBeFalsy;
+  });
+
+  it('finds one book and returns it', async () => {});
 });
