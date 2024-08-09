c265762d2a657c5feba835bfc3ad460fbffaab92
Tammy <90193520+tyfreire@users.noreply.github.com>
2024-08-09 16:38:07 +0200

    Clean up

--- src/public/books/show.js
+++ src/public/books/show.js
@@ -1,3 +1,6 @@
+const template = document.getElementById('bookTemplate').content;
+const bookTableBodyEl = document.getElementById('bookTableBody');
+
 function get_id_from_url() {
   const urlParams = new URLSearchParams(window.location.search);
   const id = urlParams.get('id');
@@ -11,9 +14,18 @@ async function fetch_book_by_id(id) {
   return book;
 }
 
+function populateTemplate(book) {
+  const clone = document.importNode(template, true);
+  const titleEl = clone.querySelector('th[slot=title]');
+  titleEl.textContent = book.title;
+
+  bookTableBodyEl.appendChild(clone);
+}
+
 async function main() {
   const id = get_id_from_url();
   const book = await fetch_book_by_id(id);
+  populateTemplate(book);
 }
 
 document.addEventListener('DOMContentLoaded', main);
